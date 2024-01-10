31ee411a6f612d38ee7b99ebd48481f02e865bf0
shashankssp12 <122225481+shashankssp12@users.noreply.github.com>
2024-01-10 23:11:28 +0530

    Initialized the authentication process

---
+++ VEG_REC/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+class Recipe(models.Model):
+    # id=models.AutoField()
+    user=models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    Rname=models.CharField(max_length=120)
+    description=models.TextField()
+    Rimage=models.ImageField(upload_to="RecipeImages")
+
+    #for better formatting in python shell 
+    def __str__(self) -> str:
+        return self.Rname 
+    
